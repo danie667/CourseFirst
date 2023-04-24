0c861889c6b4b0fc00ab1eeaceb4ce3d57a25473
danie667 <bombuuk@gmail.com>
2023-04-24 16:00:41 +0600

    Initial Commit

---
+++ CourseFirst/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CourseFirst
+//
+//  Created by Daniyal Abuov on 24.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
