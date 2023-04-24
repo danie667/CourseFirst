42e1e75207da77a8b6c4312797b97f7ba5017a96
danie667 <bombuuk@gmail.com>
2023-04-24 17:50:54 +0600

    Add outlets Start Button action

--- CourseFirst/ViewController.swift
+++ CourseFirst/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var HelloWorldLabel: UILabel!
+    @IBOutlet var startButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        HelloWorldLabel.isHidden = true
+        startButton.layer.cornerRadius = 10
     }
 
-
+    @IBAction func startUiButtonPressed() {
+        if HelloWorldLabel.isHidden{
+            HelloWorldLabel.isHidden = false
+            startButton.setTitle("Clear Text", for: .normal)
+        } else {
+            HelloWorldLabel.isHidden = true
+            startButton.setTitle("Show Text", for: .normal)
+        }
+        
+    }
+    
 }
 
